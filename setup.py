a9cfcac4753e21bdab72eaf5d9257f6ca8841844
leshchenko1979 <60138889+leshchenko1979@users.noreply.github.com>
2020-07-12 10:50:21 +0300

    смена версии

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name="gosnomer",
-    version="0.0.5",
+    version="0.0.6",
     author="Alexey Leshchenko",
     author_email="leshchenko@gmail.com",
     description="Исправление ручного ввода автомобильных номеров РФ",
